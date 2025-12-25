2c02c23ab1f7e19cc9e387116277cb9fecbf1449
Bedulgi999 <bedulgi999999999@gmail.com>
2025-12-26 05:55:10 +0900

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const SUPABASE_URL = "https://xfbeqkuaxirgubdvczmo.supabase.co";
-export const SUPABASE_ANON_KEY = "너의 anon key";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmYmVxa3VheGlyZ3ViZHZjem1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2NjQxMjAsImV4cCI6MjA4MjI0MDEyMH0.KQpw28WJE1QWO6jfv_nzkNhVg1xCLuNv66xBRHefpA4";
+
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
